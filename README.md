bfcaa3acb102cb830f8bd9c41cd1a1fa3e6c9263
João Marques <jon.marques9@gmail.com>
2024-11-08 21:00:44 -0400

    atualizando versão

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ProbLog_IA
-ProbLog_IA
+Trabalho 2 - Raciocínio probabilistico
+
+Um sistema de diagnóstico deve ser feito para um farol de bicicleta movido a dínamo usando uma rede bayesiana usando a linguagem prolog.
--- problog_E.pl
+++ problog_E.pl
@@ -1,9 +1,9 @@
-% Probabilidades base para a condição da rua (Str)
+% Probabilidades base para a condição da rua
 0.1::str(snow_covered).
 0.6::str(dry).
 0.3::str(wet).
 
-% Probabilidades base para o volante do dínamo desgastado (Flw)
+% Probabilidades base para o volante do dínamo desgastado
 0.2::flw.
 
 % Probabilidades condicionais para o dínamo deslizante (R) com base na condição da rua (Str) e no estado do volante (Flw)
